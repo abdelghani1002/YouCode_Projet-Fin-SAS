43e0ac6691b972f547523849e2c4c4068dac48d9
abdelghaniAit <aaittamghart8@gmail.com>
2023-09-20 18:56:08 +0100

    first commit

---
+++ app.c
@@ -0,0 +1,27 @@
+/*
+ * Task Management Application
+ * 
+ * Description:
+ * This application provides a menu-driven interface for managing tasks. 
+ * Users can add, read, modify, delete, and search for tasks.
+ * The application allows sorting tasks and provides statistics on tasks.
+ *
+ * Features:
+ * - Add new tasks with unique identifiers, titles, descriptions, deadlines, collaborators, and statuse.
+ * - Display a list of all tasks, sorted alphabetically or by deadline.
+ * - Show tasks with deadlines within the next three days.
+ * - Modify task details, including descriptions, statuses, and deadlines.
+ * - Delete tasks by their unique identifiers.
+ * - Search for tasks.
+ * - View statistics, including the total number of tasks, completed and incomplete tasks, and days until task deadlines.
+ *
+ * Author: AIT TAMGHART Abdelghani
+ * Date: 20/09/2023
+ */
+
+// Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
